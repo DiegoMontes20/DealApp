90f7e5978bf00c02ee3188f4c980778e3c05279a
Alanfg17 <alanferg123@gmail.com>
2021-10-29 02:23:28 -0500

    splashScreen, login, registro

--- app/src/main/java/mx/edu/utez/deal/Login/LoginScreen.kt
+++ app/src/main/java/mx/edu/utez/deal/Login/LoginScreen.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_login_screen.*
+import mx.edu.utez.deal.MainActivity
 import mx.edu.utez.deal.R
 import mx.edu.utez.deal.Registro.RegistroScreen
 
@@ -14,6 +15,10 @@ class LoginScreen : AppCompatActivity() {
 
         registrarse.setOnClickListener {
             startActivity(  Intent(this, RegistroScreen::class.java))
+        }
+
+        ingresar.setOnClickListener {
+            startActivity(  Intent(this, MainActivity::class.java))
         }
     }
 }
