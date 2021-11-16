e7a5d1ebaa2fdcd21b53ae736995eaaf4d2bb9b4
oscar <oscar.urueta.ou@gmail.com>
2021-11-16 13:13:36 -0600

    Fun getToken en splash

--- app/src/main/java/mx/edu/utez/deal/SplashScreen.kt
+++ app/src/main/java/mx/edu/utez/deal/SplashScreen.kt
@@ -4,6 +4,9 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.util.Log
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.messaging.FirebaseMessaging
 import mx.edu.utez.deal.Login.LoginScreen
 
 class SplashScreen : AppCompatActivity() {
@@ -18,5 +21,20 @@ class SplashScreen : AppCompatActivity() {
             startActivity(Intent(this, LoginScreen::class.java))
         }, 4000)
 
+    }
+
+    private fun getFirebaseToken(): String? {
+        var token: String? = null
+        FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener { task ->
+            if (!task.isSuccessful) {
+                Log.w("MyFirebaseMsgService", "Fetching FCM registration token failed", task.exception)
+                return@OnCompleteListener
+            }
+
+            // Get new FCM registration token
+            token = task.result
+            Log.w("MyFirebaseMsgService", "$token")
+        })
+        return token
     }
 }
