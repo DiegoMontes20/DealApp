5158afaa5ed2d4c7a5a1ed6d4ac05129100a08f6
Alanfg17 <alanferg123@gmail.com>
2021-11-24 23:18:12 -0600

    appointment, item agenda

---
+++ app/src/main/java/mx/edu/utez/deal/AppoinmentProcess/SummaryActivity.kt
@@ -0,0 +1,12 @@
+package mx.edu.utez.deal.AppoinmentProcess
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import mx.edu.utez.deal.R
+
+class SummaryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_summary)
+    }
+}
